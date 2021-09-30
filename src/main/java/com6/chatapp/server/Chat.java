d46f4c22678ca01a53cfd23cea129db8404a829f
Euseung <yousi7@naver.com>
2021-09-30 23:58:00 +0900

    chat

--- src/main/java/com6/chatapp/server/Chat.java
+++ src/main/java/com6/chatapp/server/Chat.java
@@ -1,2 +1,19 @@
-package com6.chatapp.server;public class Chat {
+package com6.chatapp.server;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Data
+@Document(collection = "chat")
+public class Chat {
+    @Id
+    private String id;
+    private String msg;
+    private String sender;
+    private String receiver;
+
+    private LocalDateTime createdAt;
 }
--- src/main/java/com6/chatapp/server/ChatRepository.java
+++ src/main/java/com6/chatapp/server/ChatRepository.java
@@ -1,2 +1,13 @@
-package com6.chatapp.server;public interface ChatRepository {
+package com6.chatapp.server;
+
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.data.mongodb.repository.Tailable;
+import reactor.core.publisher.Flux;
+
+public interface ChatRepository extends ReactiveMongoRepository<Chat,String> {
+    // Flux : 흐름이다. 데이터가 단방향이 아닌 응답을 유지하면서 데이터를 계속 교류
+    @Tailable       //  커서를 안닫고 계속 유지 하는 어노테이션
+    @Query("{sender:?0,receiver:?1}")
+    Flux<Chat>mFindBySender(String sender, String receiver);
 }
