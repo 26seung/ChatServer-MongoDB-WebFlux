2ed82f613708d4dfcba081fa3488544b22b202d6
Euseung <yousi7@naver.com>
2021-10-04 20:25:52 +0900

    back

--- chat-back/src/main/java/com6/chatapp/server/ChatRepository.java
+++ chat-back/src/main/java/com6/chatapp/server/ChatRepository.java
@@ -8,6 +8,6 @@ import reactor.core.publisher.Flux;
 public interface ChatRepository extends ReactiveMongoRepository<Chat,String> {
     // Flux : 흐름이다. 데이터가 단방향이 아닌 응답을 유지하면서 데이터를 계속 교류
     @Tailable       //  커서를 안닫고 계속 유지 하는 어노테이션
-    @Query("{sender:?0,receiver:?1}")
+    @Query("{sender: ?0, receiver: ?1}")
     Flux<Chat>mFindBySender(String sender, String receiver);
 }
